65b5e5477c8b2d0d3c8e17735cff3d89d5562d6e
Bendjelloul Noureddine <bendjelloulnoureddine@gmail.com>
2021-02-22 19:27:14 +0100

    [main]: Add Person Model

--- person/models.py
+++ person/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    first_name      = models.CharField(blank=True,null=True,max_length=255)
+    last_name       = models.CharField(blank=True,null=True,max_length=255)
+    birthday        = models.DateField(blank=True,null=True)
+    phone_number    = models.CharField(blank=True,null=True,max_length=10)
+    city            = models.CharField(blank=True,null=True,max_length=100)
+    provence        = models.CharField(blank=True,null=True,max_length=100)
+    commune         = models.CharField(blank=True,null=True,max_length=100)
+    blood_type      = models.CharField(blank=True,null=True,max_length=5)
+    
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
